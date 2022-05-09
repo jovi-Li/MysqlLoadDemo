302de978dede61d47bba7e29bf716e9cad5cebe2
lizongwei <lizongwei@ourvend.com>
2022-05-09 11:01:47 +0800

    first commit

---
+++ TestMysqlLoad/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MySql.Data.MySqlClient;
+using System.Data;
+using TestMysqlLoad;
+
+Console.WriteLine("Hello, World!");
+
+string constring = "server=rm-bp1c18j266i5jz40bwo.mysql.rds.aliyuncs.com;port=3306;database=Zeus_PIS;user=;password=;Charset=utf8mb4;ConvertZeroDatetime=true;AllowZeroDateTime=true;AllowLoadLocalInfile=true;sslmode=none;";
+
+using (MySqlConnection connection = new MySqlConnection(constring))
+{
+    MySqlTransaction? sqlTransaction = null;
+    try
+    {
+        connection.Open();
+        sqlTransaction = connection.BeginTransaction();
+
+        DataTable dt = new DataTable
+        {
+            TableName = "data"
+        };
+        dt.Columns.Add("name");
+        dt.Columns.Add("age");
+
+        for (int i = 1; i <= 100000; i++)
+        {
+            dt.Rows.Add(new Object[] { "测试数据" + i, i });
+        }
+
+        dt.ToCsv();
+
+        MyHelper.BulkLoad(connection, dt);
+
+        sqlTransaction.Commit();
+
+        Console.WriteLine("done！");
+    }
+    catch (Exception e)
+    {
+        if (sqlTransaction != null)
+        {
+            sqlTransaction.Rollback();
+        }
+        Console.WriteLine("Error：" + e.Message);
+    }
+
+}
+
+Console.ReadLine();
---
+++ TestMysqlLoad/sql.sql
@@ -0,0 +1,11 @@
+﻿
+-- ----------------------------
+-- Table structure for data
+-- ----------------------------
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000001 DEFAULT CHARSET=utf8;
